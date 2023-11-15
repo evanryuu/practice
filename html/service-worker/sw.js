612da6dddd4d22e2763a5f16fe511de04a756b54
evanryuu <evanryuu@outlook.com>
2023-11-15 21:50:45 +0800

    chore: service-worker

---
+++ html/service-worker/sw.js
@@ -0,0 +1,49 @@
+// sw.js (Service Worker)
+
+const CACHE_NAME = 'my-cache-v1';
+const urlsToCache = [
+  '/',
+  './index.html',
+  './images/cat.png',
+  // Add more URLs to cache as needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // If the resource is found in the cache, return it
+        if (response) {
+          return response;
+        }
+
+        // Otherwise, fetch the resource from the network
+        return fetch(event.request)
+          .then(response => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to use it and put it into the cache
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
