742e98ed9a346b3742176851e357dd2166255dfb
evanryuu <evanryuu@outlook.com>
2023-11-22 17:46:56 +0800

    chore: reflect xss attack

---
+++ web-safety/reflect-xss/index.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+
+var app = module.exports = express();
+
+// Register ejs as .html. If we did
+// not call this, we would need to
+// name our views foo.ejs instead
+// of foo.html. The __express method
+// is simply a function that engines
+// use to hook into the Express view
+// system by default, so if we want
+// to change "foo.ejs" to "foo.html"
+// we simply pass _any_ function, in this
+// case `ejs.__express`.
+
+app.engine('.html', require('ejs').__express);
+
+// Optional since express defaults to CWD/views
+
+app.set('views', path.join(__dirname, 'views'));
+
+// Path to our public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Without this you would need to
+// supply the extension to res.render()
+// ex: res.render('users.html').
+app.set('view engine', 'html');
+
+// Dummy users
+var users = [
+  { name: 'tobi', email: 'tobi@learnboost.com' },
+  { name: 'loki', email: 'loki@learnboost.com' },
+  { name: 'jane', email: 'jane@learnboost.com' }
+];
+
+app.get('/', function(req, res){
+  res.render('users', {
+    users: users,
+    title: "EJS example",
+    header: "Some users",
+    xss: req.query.xss || '' // core
+    // try http://localhost:3001/?xss=%3Cscript%3Ealert(%27attack%27)%3C/script%3E
+  });
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3001);
+  console.log('Express started on port 3001');
+}
