8e38e5f22d46e94b0985f15003dcb15d6e50c870
evanryuu <evanryuu@outlook.com>
2023-12-11 22:33:02 +0800

    chore: finish 2.4

---
+++ rust/course.rs/ch2/ch2_4/src/2.4.5/q1.rs
@@ -0,0 +1,9 @@
+// https://github.com/sunface/rust-by-practice/blob/master/solutions/compound-types/array.md
+
+fn main() {
+  // 使用合适的类型填空
+  let arr: __ = [1, 2, 3, 4, 5];
+
+  // 修改以下代码，让它顺利运行
+  assert!(arr.len() == 4);
+}
